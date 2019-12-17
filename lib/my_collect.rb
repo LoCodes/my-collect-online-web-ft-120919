8a3ed98b53350e8acccaa694e15b6f4936af16f4
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-17 04:40:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,13 +7,3 @@ def hello(array)
   end
   collection
 end
-
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
-end
