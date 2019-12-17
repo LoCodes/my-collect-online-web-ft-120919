8fd295b060a4a2c189c5b356ba05749b8cc63707
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-17 04:38:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,19 +1,9 @@
-def hello(array)
-  i = 0 
-  collection= []
+def my_collect(array)
+  i = 0
+  collect = []
   while i < array.length
-    collection << yield (array[i])
-    i += 1
+    collect << yield(array[i])
+    i+=1
   end
-  collection
-end
-
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
+  collect
 end
